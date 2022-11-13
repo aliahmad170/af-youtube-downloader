b4127a08105e6129157f7b1c8df4d0640e9bf76f
aliahmad170 <aliahmad170>
2022-11-13 18:32:53 +0200

    final

--- index.php
+++ index.php
@@ -1,40 +1,96 @@
 <?php require './function.php'; $error = "";?>
 <html lang="en" class="h-100">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Download YouTube video</title>
-    <!-- Font-->
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400&display=swap" rel="stylesheet">
-    <!-- Bootstrap core CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.css" rel="stylesheet">
-    <style>
-        body {
-            font-family: 'Montserrat', sans-serif;
-        }
-        .formSmall {
-            width: 700px;
-            margin: 20px auto 20px auto;
-        }
-    </style>
+   
 
+    
+    
+    
+    
+   
+   
+    <link href="img/favicon.ico" rel="icon">
+
+   
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css"rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href=" {{ asset('C:\Users\ali-fhas\Desktop\laravel course\Youtube-Downloader\public\assets\lib\owlcarousel\assets\owl.carousel.min.css') }}"rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="{{asset('C:\Users\ali-fhas\Desktop\laravel course\Youtube-Downloader\public\assets\css\style.css')}}" rel="stylesheet">
+
+    <link rel="shortcut icon" href="{{asset('assets\img\images.jpeg')}}">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4645016582538623"
+     crossorigin="anonymous"></script>
+
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400&display=swap" rel="stylesheet">
+<!-- Bootstrap core CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.css" rel="stylesheet">
+<style>
+    body {
+        font-family: 'Montserrat', sans-serif;
+    }
+    .formSmall {
+        width: 700px;
+        margin: 20px auto 20px auto;
+    }
+</style>
+
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Untitled</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    
 </head>
+
+<div class="container-fluid p-0">
+        <nav class="navbar navbar-expand-lg bg-white navbar-light py-3 py-lg-0 px-lg-5">
+            <a href="index.html" class="navbar-brand ml-lg-3">
+                <h1 class="m-0 text-uppercase text-primary">AF Youtube Downloader</h1>
+            </a>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-between px-lg-3" id="navbarCollapse">
+                <div class="navbar-nav mx-auto py-0">
+                    
+                     <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">language</a>
+                        
+                            
+                        
+                    </div> 
+                  
+                </div>
+                
+            </div>
+        </nav>
+    </div>
+    
+   
+            <h6 class="text-white display-6 mb-5">download any video from Youtube for free</h6>
+           
 <body>
     <div class="container">
         <form method="post" action="" class="formSmall">
             <div class="row">
                 <div class="col-lg-12">
-                    <h7 class="text-align"> Download YouTube Video</h7>
+                    <h2 class="text-align"> Download YouTube Video</h2>
                 </div>
                 <div class="col-lg-12">
                     <div class="input-group">
-                        <input type="text" class="form-control" name="video_link" placeholder="Paste link.. e.g. https://www.youtube.com/watch?v=5cpIZ8zHHXw" <?php if(isset($_POST['video_link'])) echo "value='".$_POST['video_link']."'"; ?>>
+                        <input type="text" class="form-control" name="video_link" placeholder="Please Paste link.. " <?php if(isset($_POST['video_link'])) echo "value='".$_POST['video_link']."'"; ?>>
                         <span class="input-group-btn">
                         <button type="submit" name="submit" id="submit" class="btn btn-primary">Go!</button>
                       </span>
-                    </div><!-- /input-group -->
+                    </div>
                 </div>
-            </div><!-- .row -->
+            </div>
         </form>
 
         <?php if(isset($_POST['submit'])): ?>
@@ -130,7 +186,9 @@ $thumbnail = end($thumbnails)->url;
                     </tr>
                 <?php endforeach; ?>
             </table>
+            <h5>scroll down for more format </h5>
         </div>
+        
     </div>
     
     <!-- Your code here for additional formats -->
@@ -166,12 +224,71 @@ $thumbnail = end($thumbnails)->url;
     </div>
 </div>
 
-    <!-- end -->
+   
     
 <?php endif; ?>
 
 
 <?php endif; ?>
     </div>
+    
+    </div>
+    </div>
+    
+
+
+   
+    
+
+
+
+    
+   
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+
+   
+
+
+<div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>AF</h3>
+                        <ul>
+                        <li>   <p><i class="fa fa-phone-alt mr-2"></i>+961 81 528 892</p></li>
+                            <li>      <p><i class="fa fa-envelope mr-2"></i>aliahmadfahs17@gmail.com</p></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>About</h3>
+                        <ul>
+                            
+                            <li> <a class="text-white-50 mb-2" href="{{route('privacy')}}"><i class="fa fa-angle-right mr-2"></i>Privacy Policy</a></li>
+                           <li> <a class="text-white-50" href="{{route('contact')}}"><i class="fa fa-angle-right mr-2"></i>Contact</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3>AF</h3>
+                        <p>downlad all youtube video free....</p>
+                    </div>
+                    <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
+                </div>
+                <p class="m-7">Copyright &copy; <a class="text-black" href="#">AF Youtube Downloader </a>. All Rights Reserved.
+            </div>
+        </footer>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
 </body>
+
+</html>
 </html>
